0f97a00cca2dcb0121b7478f28075ea1f90dc552
unknown <joshluffy99@gmail.com>
2022-09-29 17:50:50 +0800

    make some gamestartscene(demo)

--- Assets/Scripts/GameStartScript.cs
+++ Assets/Scripts/GameStartScript.cs
@@ -8,9 +8,12 @@ public class GameStartScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        iTween.MoveTo(ObjectArray[0], iTween.Hash("x", 0, "time", 1, "delay", 1, "easetype", iTween.EaseType.easeOutBounce
-        ,"oncomplete", "Test", "oncompletetarget",gameObject));
-        iTween.MoveTo(ObjectArray[1], iTween.Hash("y", 0, "time", 1, "easetype", iTween.EaseType.easeOutBounce));
+        iTween.MoveTo(ObjectArray[0], iTween.Hash("y", 0, "time", 1, "easetype", iTween.EaseType.easeOutBounce));
+        iTween.MoveTo(ObjectArray[1], iTween.Hash("x", 10, "easetype",iTween.EaseType.easeOutBounce, "delay", 0.5f));
+        iTween.MoveTo(ObjectArray[2], iTween.Hash("x", -10, "easetype",iTween.EaseType.easeOutBounce, "delay", 1, "oncomplete", "after_join", "oncompletetarget",gameObject));
+        iTween.MoveTo(ObjectArray[3], iTween.Hash("x", 0, "time", 1, "delay", 1.5, "easetype", iTween.EaseType.easeOutBounce
+        ));
+        iTween.MoveTo(ObjectArray[4], iTween.Hash("x", 5, "time", 20, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.linear));
     }
 
     // Update is called once per frame
@@ -19,9 +22,10 @@ public class GameStartScript : MonoBehaviour
         
     }
     
-    public void Test()
+    public void after_join()
     {
-        Debug.Log("Test");
+        iTween.PunchRotation(ObjectArray[1], iTween.Hash("z", 2, "time", 0.5f, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.linear));
+        iTween.PunchRotation(ObjectArray[2], iTween.Hash("z", 2, "time", 0.5f, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.linear));
     }
     
 }
